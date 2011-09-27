1d923f742e9b2e52245e636999f5dc60ab64b2fd
Ruben Verborgh <ruben.verborgh@gmail.com>
2011-09-27 14:33:45 +0200

    Added options.

--- eye.js
+++ eye.js
@@ -5,25 +5,63 @@ var spawn = require('child_process').spawn;
 var commentRegex = /^#.*$\n/mg,
     errorRegex = /^\*\* ERROR \*\*.*$/m;
     
+var noArgOptions = ['nope', 'noBranch', 'noDistinct', 'noQvars',
+                    'noQnames', 'quiet', 'quickFalse', 'quickPossible',
+                    'quickAnswer', 'think', 'ances', 'ignoreSyntaxError',
+                    'pcl', 'strings', 'debug', 'profile', 'version', 'help',
+                    'pass', 'passAll'];
+
 var eye = module.exports = {
+  defaults: {
+    nope: true,
+    pass: false,
+    data: []
+  },
+  
   pass: function (data, onOutput, onError) {
-    return this.execute({ data: data }, onOutput, onError);
+    return this.execute({ data: data, pass: true }, onOutput, onError);
   },
   
   execute: function (params, onOutput, onError) {
-    var output = "",
+    // add default parameters if applicable
+    params = params || {};
+    for(var prop in this.defaults) {
+      if (this.defaults.hasOwnProperty(prop) && typeof(params[prop]) === 'undefined') {
+        params[prop] = this.defaults[prop];
+      }
+    }
+    
+    // set EYE commandline options according to parameters
+    var options = [];
+    noArgOptions.forEach(function (name) {
+      if (params[name]) {
+        options.push('--' + name.replace(/([A-Z])/g, '-$1').toLowerCase());
+      }
+    });
+    
+    // add data URIs
+    params.data.forEach(function (url) {
+      if (url.match(/^http:\/\/[^(?:localhost)(?:127\.)]/)) {
+        options.push(url);
+      }
+    });
+    
+    // start EYE
+    var eye = spawn('eye', options),
+        output = "",
         error = "";
     
-    var eye = spawn('eye', ['--pass', '--nope'].concat(params.data || []))
-
+    // capture stdout
     eye.stdout.on('data', function (data) {
       output += data;
     });
-
+    
+    // capture stderr
     eye.stderr.on('data', function (data) {
       error += data;
     });
-
+    
+    // handle exit event by reporting output or error
     eye.on('exit', function (code) {
       var errorMatch = error.match(errorRegex);
       if (!errorMatch) {
