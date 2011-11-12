cacebc3718d6fb1e4173da16f701624dbc4fa5f1
Ruben Verborgh <ruben.verborgh@gmail.com>
2011-11-12 16:25:44 +0100

    Added vows for EyeServer.

--- eyeserver.js
+++ eyeserver.js
@@ -1,16 +1,43 @@
 var express = require('express'),
     eye = require('./eye');
 
-function EyeServer() {
+function EyeServer(options) {
+  options = options || {};
+  
   eyeServer = express.createServer();
   eyeServer.constructor = EyeServer;
   eyeServer.prototype = EyeServer.prototype;
   
   eyeServer.get(/^\/$/, function (req, res, next) {
-    var query = req.query,
-        data = req.query.data.split(',');
+    var reqParams = req.query,
+        data = reqParams.data || [],
+        settings = {};
+    
+    // collect data and data URIs
+    if(typeof(data) === 'string')
+      settings.data = data.split(',');
+    else {
+      settings.data = [];
+      // inspect all data parameters in request parameters
+      data.forEach(function(item){
+        if(!item.match(/^https?:\/\//))
+          // item is N3 data – push it
+          settings.data.push(item);
+        else
+          // item is list of URIs – push each of them
+          settings.data.push.apply(settings.data, item.split(','));
+      });
+    }
+    
+    // do a reasoner pass by default
+    settings.pass = true;
 
-    eye.pass(data,
+    // add debug information if requested
+    if(options.debug)
+      settings.originalUrl = req.originalUrl;
+
+    // execute the reasoner and return result or error
+    (options.eye || eye).execute(settings,
       function (result) {
         res.header('Content-Type', 'text/n3');
         res.send(result + '\n');
--- test/eyeserver-test.js
+++ test/eyeserver-test.js
@@ -1,5 +1,6 @@
 var vows = require('vows'),
-    should = require('should');
+    should = require('should'),
+    request = require('request');
 var eyeserver = require('../eyeserver.js');
 
 vows.describe('EyeServer').addBatch({
@@ -17,5 +18,80 @@ vows.describe('EyeServer').addBatch({
     'should be an EyeServer constructor': function (eyeserver) {
       new eyeserver().constructor.should.eql(eyeserver);
     }
+  },
+  
+  'An EyeServer instance': {
+    topic: function() {
+      server = new eyeserver({ eye: eyeDummy, debug: true });
+      server.listen(3000);
+      return server;
+    },
+    
+    'receiving a request to /':
+      respondsWith(200, 'text/n3', 'without data', { data: [], pass: true }),
+    
+    'receiving a request to /?data=http%3A%2F%2Fex.org%2Ferror':
+      respondsWith(400, 'text/plain', 'and receive an error', { data: ['http://ex.org/error'], pass: true }),
+    
+    'receiving a request to /?data=http%3A%2F%2Fex.org%2F1':
+      respondsWith(200, 'text/n3', 'with one URI', { data: ['http://ex.org/1'], pass: true }),
+    
+    'receiving a request to /?data=http%3A%2F%2Fex.org%2F1,http%3A%2F%2Fex.org%2F2':
+      respondsWith(200, 'text/n3', 'with two URIs', { data: ['http://ex.org/1', 'http://ex.org/2'], pass: true }),
+    
+    'receiving a request to /?data=http%3A%2F%2Fex.org%2F1&data=http%3A%2F%2Fex.org%2F2':
+      respondsWith(200, 'text/n3', 'with two URIs', { data: ['http://ex.org/1', 'http://ex.org/2'], pass: true }),
+    
+    'receiving a request to /?data=http%3A%2F%2Fex.org%2F1&data=http%3A%2F%2Fex.org%2F2,http%3A%2F%2Fex.org%2F3':
+      respondsWith(200, 'text/n3', 'with three URIs', { data: ['http://ex.org/1', 'http://ex.org/2', 'http://ex.org/3'], pass: true })
   }
 }).export(module);
+
+var eyeDummy = {
+  execute: function (options, onOutput, onError) {
+    var path = options.originalUrl;
+    delete options.originalUrl;
+    this.options[path] = options;
+    
+    if(this.shouldSucceed[path])
+      onOutput('output');
+    else
+      onError('error');
+  },
+  options: {},
+  shouldSucceed: {}
+}
+
+function respondsWith(status, contentType, executionDescription, executeArguments) {
+  var path, shouldSucceed = (status >= 200 && status <= 299);
+  var context = {
+    topic: function () {
+      path = this.context.name.match(/[^ ]+$/)[0];
+      eyeDummy.shouldSucceed[path] = shouldSucceed;
+      request('http://localhost:3000' + path, this.callback);
+    }
+  };
+  
+  context['should respond with status ' + status] = function(error, response, body) {
+    response.statusCode.should.eql(status);
+  }
+  
+  context['should respond with Content-Type ' + contentType] = function(error, response, body) {
+    response.headers.should.have.property('content-type', contentType)
+  }
+  
+  if(shouldSucceed)
+    context['should return the Eye output'] = function(error, response, body) {
+      body.should.eql('output\n');
+    }
+  else
+    context['should return the Eye error'] = function(error, response, body) {
+      body.should.eql('error\n');
+    }
+  
+  context['should execute Eye ' + executionDescription] = function (error, response, body) {
+    eyeDummy.options[path].should.eql(executeArguments);
+  }
+  
+  return context;
+}
